f8a44e17979d95b443f90cc889ff8d075615e9b0
KriviDori <99684290+Hirannis@users.noreply.github.com>
2022-04-25 20:04:50 +0200

    Add files via upload

---
+++ butorok15/Fabutorok.java
@@ -0,0 +1,13 @@
+package butorok15;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+        
+public abstract class Fabutorok {
+    static int ertek = 50000;
+    static double suly;
+    
+}
---
+++ butorok15/Karpitozottbutorok.java
@@ -0,0 +1,16 @@
+package butorok15;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.text.StringCharacterIterator;
+        
+public abstract class Karpitozottbutorok {
+    static int ertek = 50000;
+    static char[] anyag;
+
+    
+    
+}
